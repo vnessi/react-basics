754dc58202a8374e782d0270bb2f3b1f33891e08
Victor Nessi <v.nessi@salesforce.com>
2022-07-08 11:31:17 -0300

    publish-project

---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Link to="/registry">Click here to go to registry</Link>
+        </div>
+    )
+}
+
+export default Home;
